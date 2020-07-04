86cfea936d544599cf7770968e9f7ce5a17f0348
Daniel Carr <danielcarr@MacBook-Pro.attlocal.net>
2020-07-04 10:29:57 -0700

    updated_test

--- test/test_app.js
+++ test/test_app.js
@@ -1,23 +1,19 @@
-import { utils } from 'wonderland-commons';
 import App from 'app';
 import config from 'config';
 import request from 'supertest';
 
-const { safely, conditionalOnEnv } = utils;
+// todo: create docker-compose test environment so we can run the server
 
 describe('app bootstrap', () => {
     let app = null;
 
-    before(
-        conditionalOnEnv('test-docker', async () => {
-            app = await safely(App.start);
-        })
-    );
+    before(async () => {
+        app = await App.start();
+    });
 
     after(async () => {
         if (app) {
-            await safely(app.shutdown);
-            await new Promise(resolve => setTimeout(resolve, 1000));
+            app.shutdown();
         }
     });
 
@@ -30,18 +26,6 @@ describe('app bootstrap', () => {
     it('should respond to health check', async () => {
         await request(app.server)
             .get(`${config.prefix}/health`)
-            .expect(200);
-    });
-
-    it('should respond to heartbeat', async () => {
-        await request(app.server)
-            .get(`${config.prefix}/heartbeat`)
-            .expect(200);
-    });
-
-    it('should return config', async () => {
-        await request(app.server)
-            .get(`${config.prefix}/config`)
             .expect(200);
     });
 });
